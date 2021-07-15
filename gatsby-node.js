eb29809b804d914e8f03f0ddc66c1046ef54b097
Carolina Cordero <ccalvarez@gmail.com>
2021-07-15 11:50:11 -0600

    refactor: calls createSchemaCustomization instead of sourceNodes
    To adhere to https://www.gatsbyjs.com/docs/reference/graphql-data-layer/schema-customization

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,13 @@
-exports.sourceNodes = ({ actions }) => {
-  actions.createTypes(`
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
     type hasura_parties implements Node @dontInfer {
       id: Int!,
       name: String!,
       idToString: String,
     }
-  `)
+  `
+  createTypes(typeDefs)
 }
 
 exports.createResolvers = ({ createResolvers }) => {
